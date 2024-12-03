b2ba228b38016268bf78816180c397f72933f956
Пользователь <jibekisabeva>
2024-12-03 14:38:11 +0500

    first commit

---
+++ exxx6.py
@@ -0,0 +1,5 @@
+
+surname=input("familyangizni kiriting:   ")
+num_of_letters=len(surname)
+for _ in range(num_of_letters):
+    print(surname)
---
+++ exxxx4.py
@@ -0,0 +1,4 @@
+dollarkursi=12800
+oyliksomda=float(input("oiligizni kiriting:   "))
+oilikdollarda=oyliksomda/dollarkursi
+print(f"oiligingiz dollarda : {oilikdollarda: 2f}  AQSH dollar")
